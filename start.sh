7a27a9db990d1f23937bc8e24b0f0b355e4b6238
Ernest44tech <ernestoesq14@gmail.com>
2025-05-08 15:58:57 -0600

    fix: usar artisan serve en lugar de php -S

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Navega a la carpeta del proyecto
 cd /app
 
-# Da permisos si es necesario
 chmod -R 775 storage bootstrap/cache
 
-# Sirve Laravel en el puerto 8080 desde la carpeta public
-php -S 0.0.0.0:8080 -t public
+php artisan serve --host=0.0.0.0 --port=8080
